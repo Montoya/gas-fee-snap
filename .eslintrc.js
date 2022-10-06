1f30b5bc8c9c47a863afd402ae84e6e98a18be09
Christian Montoya <christian.montoya@consensys.net>
2022-10-06 09:40:43 -0400

    Updated config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,41 @@
 module.exports = {
   root: true,
+  parserOptions: {
+    sourceType: 'module',
+  },
 
   extends: ['@metamask/eslint-config'],
 
   overrides: [
     {
-      files: ['*.js'],
-      parserOptions: {
-        sourceType: 'script',
-      },
-      globals: {
-        wallet: 'readonly',
-      },
+      files: ['**/*.js'],
       extends: ['@metamask/eslint-config-nodejs'],
     },
 
     {
-      files: ['*.test.js'],
+      files: ['**/*.{ts,tsx}'],
+      extends: ['@metamask/eslint-config-typescript'],
+      rules: {
+        '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+      },
+    },
+
+    {
+      files: ['**/*.test.ts', '**/*.test.js'],
       extends: ['@metamask/eslint-config-jest'],
+      rules: {
+        '@typescript-eslint/no-shadow': [
+          'error',
+          { allow: ['describe', 'expect', 'it'] },
+        ],
+      },
     },
   ],
 
-  ignorePatterns: ['!.eslintrc.js', '!.prettierrc.js', 'dist/'],
+  ignorePatterns: [
+    '!.prettierrc.js',
+    '**/!.eslintrc.js',
+    '**/dist*/',
+    '**/*__GENERATED__*',
+  ],
 };
